66c280ea81e01ff443d060973ddbfb78d92eb059
jasonpaulos <jasonpaulos@users.noreply.github.com>
2022-05-24 20:27:59 +0000

    Deploying to gh-pages from @ algorand/walletconnect-example-dapp@e68f39822cc6171124497ce4ca4ad1bdcf297914 🚀

--- precache-manifest.95a309baa06fe67095c22ffbcedeb97d.js
+++ precache-manifest.95a309baa06fe67095c22ffbcedeb97d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ae320874845829e8a725",
-    "url": "/walletconnect-example-dapp/static/js/main.ae320874.chunk.js"
+    "revision": "9b1e8e8043aca5d32cd5",
+    "url": "/walletconnect-example-dapp/static/js/main.9b1e8e80.chunk.js"
   },
   {
-    "revision": "bf6039d48c9174c0a168",
-    "url": "/walletconnect-example-dapp/static/js/1.bf6039d4.chunk.js"
+    "revision": "507e8f97cb4133b1f3b5",
+    "url": "/walletconnect-example-dapp/static/js/1.507e8f97.chunk.js"
   },
   {
     "revision": "b9fb507d40c767f08e4e",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/walletconnect-example-dapp/static/media/algo.6c6f52b0.svg"
   },
   {
-    "revision": "3b41cfee63d43bb1333e45d8d7cad5db",
+    "revision": "398500897def5f86cf45b5b1ca10ad77",
     "url": "/walletconnect-example-dapp/index.html"
   }
 ];
